17cb7871664eebeb6b6ff9c35fffda1c22564705
Zeca <Zeca@192.168.0.167>
2018-05-22 20:33:39 -0300

    Inserção do GreenHouse Control (funciona como teste da Controller)

--- StarWars/src/br/usp/ime/paca/mac0321/projetodupla_Maria_Tamy/StarWars/main.java
+++ StarWars/src/br/usp/ime/paca/mac0321/projetodupla_Maria_Tamy/StarWars/main.java
@@ -1,10 +1,16 @@
 package br.usp.ime.paca.mac0321.projetodupla_Maria_Tamy.StarWars;
 
+
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		/*  GreenHouse Controls -- classe para testar o Controller
+		GreenHouseControls gc = new GreenHouseControls();
+		long tm = System.currentTimeMillis();
+		gc.addEvent(gc.new Restart(tm));
+		gc.run();
+		*/
 	}
 
 }
